f11763cb568494efc214e60ea211343cebf01b0b
William Bogans <williambogans98@yahoo.com>
2021-04-10 23:14:37 -0400

    Adds halfway finished project

--- 2.chat_app/app/controllers/chatrooms_controller.rb
+++ 2.chat_app/app/controllers/chatrooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomsController < ApplicationController
   def index 
+    @messages = Message.all
   end 
 
   def new 
--- 2.chat_app/db/seeds.rb
+++ 2.chat_app/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username:"Will0101",password:"test12345")
+User.create(username:"Will0102",password:"test12345")
+User.create(username:"Will0103",password:"test12345")
+User.create(username:"Will0104",password:"test12345")
