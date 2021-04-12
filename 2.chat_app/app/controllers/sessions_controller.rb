56ce537ac76344eb8cbac57b0f666b241c87fc13
William Bogans <williambogans98@yahoo.com>
2021-04-12 19:01:04 -0400

    Adds restrictions to chat_app

--- 2.chat_app/app/controllers/sessions_controller.rb
+++ 2.chat_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,34 @@
-class SessionsController < ActionController::Base
-  def new
+class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+
+  def new 
   end 
-end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are now logged in"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error with your login details"
+      render "new" 
+    end 
+  end
+   
+  def destroy 
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+
+
+  private 
+  def logged_in_redirect 
+    if logged_in?
+     flash[:error] = "You are already logged in"
+     redirect_to root_path
+    end 
+  end 
+   
+end 
