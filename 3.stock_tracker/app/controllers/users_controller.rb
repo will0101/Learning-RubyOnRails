a2cfc3659775c5c09bfe7d7aa172a594a72b474e
William Bogans <williambogans98@yahoo.com>
2021-04-29 17:29:53 -0400

    Finishes the stock tracker app

--- 3.stock_tracker/app/controllers/users_controller.rb
+++ 3.stock_tracker/app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
 
   def my_portfolio
     @tracked_stocks = current_user.stocks 
+    @user = current_user
   end
 
   def friends_list
     @friends_list = current_user.friends 
   end 
+
+  def show
+    @user = User.find(params[:id]) 
+    @tracked_stocks = @user.stocks
+  end
 
   def search_for_friend
     if params[:friend].present?
